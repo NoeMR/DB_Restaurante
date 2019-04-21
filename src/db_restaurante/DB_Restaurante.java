1480f2f957472d6495cb9243eed4e15c2b3c3f8f
Noé Francisco Martinez <martinezruiznoefrancisco@gmail.com>
2019-04-20 19:40:16 -0600

    Creación de la interfaz gráfica 1

---
+++ src/db_restaurante/DB_Restaurante.java
@@ -0,0 +1,9 @@
+package db_restaurante;
+public class DB_Restaurante {
+
+    public static void main(String[] args) {
+        Principal v1 = new Principal(); 
+        v1.setVisible(true);
+    }
+    
+}
